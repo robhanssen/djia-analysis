42b58fd7ba1876c7c5354f3dc7ead331d7ee9cc9
Rob Hanssen <rob.hanssen@outlook.com>
2022-05-20 15:36:47 -0400

    updated code

---
+++ functions.r
@@ -0,0 +1,29 @@
+
+period_to_dates <- function(pd) {
+    daterange <- floor_date(
+        seq(
+            as.Date(int_start(pd)),
+            today() + years(1),
+            by = "month"
+        ),
+        unit = "month"
+    )
+    tibble(date = daterange)
+}
+
+
+predict_index <- function(tbl, daterange) {
+    tbl %>%
+        filter(date %within% daterange) %>%
+        group_by(index) %>%
+        nest() %>%
+        mutate(model = map(data, ~ lm(value ~ date, data = .))) %>%
+        mutate(modeldata = map(
+            model,
+            ~ broom::augment(.x,
+                interval = "prediction",
+                newdata = period_to_dates(daterange)
+            )
+        )) %>%
+        unnest(modeldata)
+}
---
+++ stock_index_analysis.r
@@ -0,0 +1,93 @@
+library(tidyverse)
+library(lubridate)
+library(patchwork)
+theme_set(theme_light())
+
+source("functions.r")
+
+djia <- read_csv("source/djia.csv")
+sp500 <- read_csv("source/sp500.csv")
+# nasdaq <- read_csv("source/nasdaq.csv")
+
+stockindex <-
+    inner_join(djia, sp500, by = "date") %>%
+    # inner_join(nasdaq, by = "date") %>%
+    mutate(across(!date, as.numeric)) %>%
+    pivot_longer(!date, values_to = "value", names_to = "index") %>%
+    filter(!is.na(value))
+
+train_1 <- ymd("2013-06-18") %--% ymd("2015-03-06")
+train_2 <- ymd("2017-10-01") %--% ymd("2019-10-10")
+
+date_1 <- period_to_dates(train_1)
+date_2 <- period_to_dates(train_2)
+
+model_1 <- predict_index(stockindex, train_1)
+model_2 <- predict_index(stockindex, train_2)
+
+color <- c(
+    "djia" = "darkblue",
+    "sp500" = "darkgreen",
+    "nasdaq" = "darkred"
+)
+
+stockindex %>%
+    ggplot() +
+    aes(date, value, color = index) +
+    geom_point(alpha = .2, size = .2) +
+    geom_line(
+        data = model_1,
+        aes(y = .fitted, color = index),
+        lty = 1,
+        size = .25
+    ) +
+    geom_line(
+        data = model_2,
+        aes(y = .fitted, color = index),
+        lty = 1,
+        size = .25
+    ) +
+    geom_ribbon(
+        data = model_1,
+        aes(
+            y = NULL,
+            ymin = .lower,
+            ymax = .upper,
+            fill = index,
+            color = NULL
+        ),
+        alpha = .1,
+    ) +
+    geom_ribbon(
+        data = model_2,
+        aes(
+            y = NULL,
+            ymin = .lower,
+            ymax = .upper,
+            color = NULL,
+            fill = index
+        ),
+        alpha = .1,
+    ) +
+    scale_x_date(
+        date_breaks = "2 year",
+        date_labels = "%Y",
+        minor_breaks = NULL
+    ) +
+    scale_y_continuous(
+        breaks = c(
+            seq(0, 5e3, 5e2),
+            seq(10e3, 50e3, 5e3)
+        ),
+        label = scales::comma_format()
+    ) +
+    labs(
+        x = "Date",
+        y = "Stock Index"
+    ) +
+    theme(legend.position = "none") +
+    facet_wrap(~index, scales = "free_y") +
+    scale_color_manual(values = color) + 
+    scale_fill_manual(values = color) 
+
+ggsave("graphs/modelpredictions.png", width = 12, height = 6)
