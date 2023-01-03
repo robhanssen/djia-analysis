325326fcac927e32a41e5fc6590e17499dec5136
Rob Hanssen <rob.hanssen@outlook.com>
2023-01-03 08:42:50 -0500

    adjust initial fit parameters

--- wiggles.r
+++ wiggles.r
@@ -39,7 +39,7 @@ sinemodel <- function(tbl) {
         data = tbl,
         trace = TRUE,
         rel_residue ~ a0 * sin((time - a1) / a2),
-        start = list(a0 = 1, a1 = 50, a2 = 66), 
+        start = list(a0 = 1, a1 = 50, a2 = 16), 
         control = nls.control(maxiter = 300)
     )
 }
